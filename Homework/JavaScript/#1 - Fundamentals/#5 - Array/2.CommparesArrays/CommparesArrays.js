a4486930bf5e5789ce50b86738794b902aa64b02
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-02 20:52:36 +0300

    .init

--- Homework/JavaScript/#1 - Fundamentals/#5 - Array/2.CommparesArrays/CommparesArrays.js
+++ Homework/JavaScript/#1 - Fundamentals/#5 - Array/2.CommparesArrays/CommparesArrays.js
@@ -6,25 +6,25 @@ function CommparesArrays() {
         i,
         len = Math.min(strOne.length, strTwo.length);
 
-    for (i = 0; i < len; i++) {       
-        if (strOne[i] > strTwo[i]) {
-            document.getElementById("result").innerHTML = "The Second Word is forward on Lexicography";
-            break;
-        }
-        else if (strTwo[i] > strOne[i]) {
-            document.getElementById("result").innerHTML = "The First Word is forward on Lexicography";
-            break;
-        }
-        else {
-            if (strOne.length > strTwo.length) {
+    if (strOne == strTwo) {
+        document.getElementById("result").innerHTML = "First Word = Second Word";
+    }
+    else {
+        for (i = 0; i < len; i++) {
+            if (strOne[i] > strTwo[i]) {
                 document.getElementById("result").innerHTML = "The Second Word is forward on Lexicography";
+                break;
             }
-            else if (strOne.length < strTwo.length) {
+            else if (strTwo[i] > strOne[i]) {
                 document.getElementById("result").innerHTML = "The First Word is forward on Lexicography";
-            }
-            else if (strOne.length === strTwo.length) {
-                document.getElementById("result").innerHTML = "First Word = Second Word";
+                break;
             }
         }
-    }   
+        if (strOne.length > strTwo.length) {
+            document.getElementById("result").innerHTML = "The Second Word is forward on Lexicography";
+        }
+        else if (strOne.length < strTwo.length) {
+            document.getElementById("result").innerHTML = "The First Word is forward on Lexicography";
+        }
+    }    
 }
