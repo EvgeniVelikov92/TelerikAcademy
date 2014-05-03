ade3d934c8e3e9e9b87691a2fdd5e0978b9712a6
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-03 23:23:49 +0300

    .init

--- Homework/JavaScript/#1 - Fundamentals/#1 - Data Types and Variables/1.PossibleJavaScriptLiterals/PossibleJavaScriptLiterals.js
+++ Homework/JavaScript/#1 - Fundamentals/#1 - Data Types and Variables/1.PossibleJavaScriptLiterals/PossibleJavaScriptLiterals.js
@@ -1,2 +1,11 @@
-// Assign all the possible JavaScript literals to different variables.
+// Assign all thå possible JavaScript literals to different variables.
 
+var intLit = 1;
+var stringLit = "Georgi";
+var floatVar = 2.356;
+var objLit = {
+    name: "Ivan",
+    age: 20,
+};
+var arrayLit = [1, 2, 3];
+var functionLit = function func() { alert("First Task!") };
