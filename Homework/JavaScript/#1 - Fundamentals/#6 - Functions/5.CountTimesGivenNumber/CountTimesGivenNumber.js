87844c5c704600021a9df0be3c3960d2e768a062
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-04 21:53:14 +0300

    .init

--- Homework/JavaScript/#1 - Fundamentals/#6 - Functions/5.CountTimesGivenNumber/CountTimesGivenNumber.js
+++ Homework/JavaScript/#1 - Fundamentals/#6 - Functions/5.CountTimesGivenNumber/CountTimesGivenNumber.js
@@ -1,18 +1,21 @@
 // Write a function that counts how many times given number appears in given array. Write a test function to check if the function is working correctly.
 
-function countTumesGivenNumber() {
+function getNumber() {
     var num = parseInt(document.getElementById("number").value);
+
+    countTumesGivenNumber(num);
+}
+
+function countTumesGivenNumber(number) {
     var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 7, 3, 4, 5, 7, 4, 5, 4, 4, 4, 3, 2, 3, 4];
     var i;
     var len = 1;
 
     for (i = 0; i < arr.length; i++) {
-        if (arr[i] == num) {
+        if (arr[i] == number) {
             len += 1;
         }
     }
 
     document.getElementById("result").innerHTML = "Repeat as many times is: " + len;
 }
-
-countTumesGivenNumber();
