c7dd906b2da2a52abf870e6fc7caca5f5e393e12
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-05 12:48:09 +0300

    .init

---
+++ Homework/JavaScript/#1 - Fundamentals/#6 - Functions/6.BiggerThanNeighbours/BiggerThanNeighbours.js
@@ -0,0 +1,16 @@
+// Write a function that checks if the element at given position in given array of integers is bigger than its two neighbors (when such exist).
+
+function biggerThanNeighbours() {
+    var arr = [1, 2, 3, 4, 5, 45, 6, 7, 8, 4, 6, 7, 8, 59, 3, 2, 4, 5, 3, 5, 3, 5, 7, 4, 3, 21, 3, 5, 6, 8];
+    var pos = parseInt(document.getElementById("position").value);
+    var tr;
+
+    if (arr[pos - 1] < arr[pos] && arr[pos + 1] < arr[pos]) {
+        tr = true;
+    }
+    else {
+        tr = false;
+    }
+
+    return tr;
+}
