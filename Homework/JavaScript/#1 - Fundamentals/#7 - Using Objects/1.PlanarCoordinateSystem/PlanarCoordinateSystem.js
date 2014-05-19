02729daa71b9534d3dd9db91f7cdc93aad652837
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-19 16:17:43 +0300

    .init

--- Homework/JavaScript/#1 - Fundamentals/#7 - Using Objects/1.PlanarCoordinateSystem/PlanarCoordinateSystem.js
+++ Homework/JavaScript/#1 - Fundamentals/#7 - Using Objects/1.PlanarCoordinateSystem/PlanarCoordinateSystem.js
@@ -1,42 +1,42 @@
 // Write functions for working with shapes in  standard Planar coordinate system. Points are represented by coordinates P(X, Y). Lines are represented by two points, marking their beginning and ending. L(P1(X1,Y1), P2(X2,Y2)). Calculate the distance between two points. Check if three segment lines can form a triangle.
 
-function getPoint(x, y) {
-    var point = {
-        x: x,
-        y: y
-    }
-
-    return point;
-}
+var pOne = createPoint(3, 8),
+    pTwo = createPoint(4, 5),
+    pThree = createPoint(6, 7),
+    lOne = getLine(pOne, pTwo),
+    lTwo = getLine(pTwo, pThree),
+    lThree = getLine(pThree, pOne);
+    n = calculateDistance(pThree, pTwo);
 
 function createPoint(x, y) {
-	var pointA = {
-		x: x,
-		y: y
-	},
-		pointB = {
-		x: x,
-		y: y
-	}
-	
-	return pointA, pointB;
+	return {
+        x: x,
+        y: y
+    };
+}
+
+function getLine (pointOne, pointTwo) {
+    return {
+        pointA: pointOne,
+        pointB: pointTwo
+    };
 }
 
 function calculateDistance(PointOne, PointTwo) {
-    return Math.sqrt((PointOne.x - PointTwo.x) * (PointOne.x + PointTwo.x) + (PointOne.y - PointTwo.y) * (PointOne.y - PointTwo.y));
+    return Math.sqrt(parseFloat((PointOne.x - PointTwo.x) * (PointOne.x + PointTwo.x) + (PointOne.y - PointTwo.y) * (PointOne.y - PointTwo.y)));
 }
 
-function createLines(x1, x2, y1, y2) {
-    var a = calculateDistance(4, 2, 4, 8),
-        b = calculateDistance(2, 5, 7, 3),
-        c = calculateDistance(5, 6, 1, 4);
-}
+function createTriangle(lineOne, lineTwo, lineThree) {
+    var a = calculateDistance(lineOne.pointA, lineOne.pointB),
+        b = calculateDistance(lineTwo.pointA, lineTwo.pointB),
+        c = calculateDistance(lineThree.pointA, lineThree.pointB);
 
-function createTriangle() {
-    if (true) {
+    if ((a + b) > c && (a + c) > b && (b + c) > a) {
         alert("Congratilation! You create a Triangle!");
     }
     else {
         alert("This is not a Triangle. Please try again.");
     }
 }
+
+createTriangle(lOne, lTwo, lThree);
