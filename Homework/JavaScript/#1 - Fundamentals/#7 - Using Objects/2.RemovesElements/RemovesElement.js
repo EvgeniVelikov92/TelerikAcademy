87fd9771b9586953a16b6079775186ee47db2215
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-19 15:55:37 +0300

    .init

--- Homework/JavaScript/#1 - Fundamentals/#7 - Using Objects/2.RemovesElements/RemovesElement.js
+++ Homework/JavaScript/#1 - Fundamentals/#7 - Using Objects/2.RemovesElements/RemovesElement.js
@@ -1,5 +1,8 @@
 // Write a function that removes all elements with a given value. Attach it to the array type. Read about prototype and how to attach methods.
 
+var arr = [1, 2, 1, 4, 1, 3, 4, 1, 111, 3, 2, 1, "1"],
+    finalArr = arr.remove(1);
+
 if (!Array.prototype.remove) {
     Array.prototype.remove = function (elems) {
         var newArr = [],
@@ -14,9 +17,5 @@ if (!Array.prototype.remove) {
         return newArr;
     }    
 }
-
-var arr = [1, 2, 1, 4, 1, 3, 4, 1, 111, 3, 2, 1, "1"],
-    finalArr = arr.remove(1),
-    i;
 
 console.log(finalArr);
