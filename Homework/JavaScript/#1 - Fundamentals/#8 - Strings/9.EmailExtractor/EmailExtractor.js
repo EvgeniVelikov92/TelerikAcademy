e7963db4181714115fc26346f3d10c47ded9fe20
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-20 12:26:34 +0300

    .init

--- Homework/JavaScript/#1 - Fundamentals/#8 - Strings/9.EmailExtractor/EmailExtractor.js
+++ Homework/JavaScript/#1 - Fundamentals/#8 - Strings/9.EmailExtractor/EmailExtractor.js
@@ -1,6 +1,6 @@
 // Write a function for extracting all email addresses from given text. All substrings that match the format <identifier>@<host>…<domain> should be recognized as emails. Return the emails as array of strings.
 
-var mail = "baj.ivan@yahoo.co.uk";
+var mail = "a-Z.e4@gmail.com";
 
 function emailExtract(email) {
     var valid = email.match(/([\w-\.]+@[\w]+\.?[a-zA-Z]{2,4}\.[a-zA-Z]{2,4})/g);
