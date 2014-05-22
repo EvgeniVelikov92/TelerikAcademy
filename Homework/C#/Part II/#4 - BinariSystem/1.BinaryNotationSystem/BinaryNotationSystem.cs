4a4d52235ebf3fed54eb7c4d0a3bb53aff99c4a7
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-22 16:17:41 +0300

    .init

---
+++ Homework/C#/Part II/#4 - BinariSystem/1.BinaryNotationSystem/BinaryNotationSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BinaryNotationSystem
+{
+    class BinaryNotationSystem
+    {
+        static void Main()
+        {
+            Console.Write("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int system = 2;
+            int sum = number;
+            int bin = 0;
+            string s = string.Empty;
+            while (sum == number && sum > 0)
+            {
+                sum = number / system;
+                bin = number % system;
+                number = sum;
+                s += bin.ToString();
+            }
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            Console.WriteLine(arr);
+            Console.WriteLine();
+        }
+    }
+}
