c54cd5432d3bbac3666bb2e90099ab39b6cd150f
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-23 13:52:51 +0300

    .init

---
+++ Homework/C#/Part II/ExceptionsHandling/1.CalculateSquareRoot/CalculateSquareRoot.cs
@@ -0,0 +1,38 @@
+﻿// Write a program that reads an integer number and calculates and prints its square root. If the number is invalid or negative, print "Invalid number". In all cases finally print "Good bye". 
+
+using System;
+
+namespace CalculateSquareRoot
+{
+    class CalculateSquareRoot
+    {
+        static void Main()
+        {
+            Console.Write("Please enter a number: ");
+            string number = Console.ReadLine();
+
+            SQRTNumbers(number);
+        }
+
+        static void SQRTNumbers(string number)
+        {
+            try
+            {
+                int squareNum = (int)Math.Sqrt(Int32.Parse(number));
+                Console.WriteLine("Your result is = " + squareNum);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Format! ");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Invalid Number! ");
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+        }
+    }
+}
---
+++ Homework/C#/Part II/ExceptionsHandling/2.ReadIntegerNumber/ReadIntegerNumber.cs
@@ -0,0 +1,43 @@
+﻿// Write a method ReadNumber(int start, int end) that enters an integer number in a given range [start…end]. If an invalid number or non-number text is entered, the method should throw an exception. Using this method write a program that enters 10 numbers.
+
+using System;
+
+namespace ReadIntegerNumber
+{
+    class ReadIntegerNumber
+    {
+        static void Main()
+        {
+            int start = 1;
+            int end = 100;
+
+            ReadNumber(start, end);
+        }
+
+        static void ReadNumber(int start, int end)
+        {
+            int[] arrNum = new int[10];
+            for (int i = 0; i < arrNum.Length; i++)
+            {
+                Console.Write("Input number [{0}]: ", i + 1);
+                try
+                {
+                    arrNum[i] = int.Parse(Console.ReadLine());
+                    if (arrNum[i] < start)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    else if (arrNum[i] > end)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Format! ");
+                    return;
+                }
+            }
+        }
+    }
+}
