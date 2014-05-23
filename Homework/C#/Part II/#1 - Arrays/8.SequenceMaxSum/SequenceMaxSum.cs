7b80ee5e2d4179c0865e6b25c8bb46773b7c50ed
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-23 15:41:29 +0300

    .init

---
+++ Homework/C#/Part II/#1 - Arrays/8.SequenceMaxSum/SequenceMaxSum.cs
@@ -0,0 +1,32 @@
+﻿// Write a program that finds the sequence of maximal sum in given array.
+
+using System;
+
+namespace SequenceMaxSum
+{
+    class SequenceMaxSum
+    {
+        static void Main()
+        {
+            int[] arr = { 2, 3, -6, -1, 2, -1, 6, 4, -8, 8 };
+            int bestSum = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentSum = 0;
+
+                for (int j = i; j < arr.Length; j++)
+                {
+                    currentSum += arr[j];
+                    
+                    if (currentSum > bestSum)
+                    {
+                        bestSum = currentSum;
+                    }
+                }
+            }
+
+            Console.WriteLine(bestSum);
+        }
+    }
+}
