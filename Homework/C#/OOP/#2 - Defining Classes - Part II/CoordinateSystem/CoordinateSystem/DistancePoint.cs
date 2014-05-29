96c1b4a3c6386dc1643ea45b9668b144e04a8bf1
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-05-29 13:33:38 +0300

    .init

---
+++ Homework/C#/OOP/#2 - Defining Classes - Part II/CoordinateSystem/CoordinateSystem/DistancePoint.cs
@@ -0,0 +1,22 @@
+﻿namespace CoordinateSystem
+{
+    public static class DistancePoint
+    {
+        private int x;
+        private int y;
+        private int z;
+        public Point3D point = new Point3D();
+
+        public DistancePoint()
+        {
+            this.point.X = x;
+            this.point.Y = y;
+            this.point.Z = z;
+        }
+
+        public static int CalculateDistance(int x, int y, int z)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Homework/C#/OOP/#2 - Defining Classes - Part II/CoordinateSystem/CoordinateSystem/Test.cs
@@ -0,0 +1,18 @@
+﻿namespace CoordinateSystem
+{
+    using System;
+
+    public class Test
+    {
+        static void Main()
+        {
+            Point3D point = new Point3D();
+
+            point.X = 4;
+            point.Y = 3;
+            point.Z = 2;
+
+            Console.WriteLine(point);
+        }
+    }
+}
