1a95aba55fbcdfb91ea199bc2917cca2ba240c9d
Evgeni Velikov <evgenivelikov92@gmail.com>
2014-06-23 21:29:53 +0300

    .init

--- Homework/JavaScript/#2 - DOM and UI/#9 - JQuery/1. Slider Control/javascript/slider-control.js
+++ Homework/JavaScript/#2 - DOM and UI/#9 - JQuery/1. Slider Control/javascript/slider-control.js
@@ -1,7 +1,6 @@
 // Create a slider control using jQuery. The slider can have many slides. Only one slide is visible at a time. Each slide contains HTML code. i.e. it can contain images, forms, divs, headers, links, etc… Implement functionality for changing the visible slide after 5 seconds. Create buttons for next and previous slide.
 
 function sliderControl () {
-	var $item = $('li');
 	var count = 1;
 
 	function slideShow() {
